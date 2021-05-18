b1b9f314e9a20fb7bff1308f2d39093a4f33613e
Ohyaelim <npr05080@gmail.com>
2021-05-18 09:00:12 +0900

    controller 수정하기

--- src/main/java/yl/yaelim/board/controller/BoardController.java
+++ src/main/java/yl/yaelim/board/controller/BoardController.java
@@ -2,9 +2,17 @@ package yl.yaelim.board.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import yl.yaelim.board.domain.dto.BoardDto;
+import yl.yaelim.board.domain.service.BoardService;
 
 @Controller
 public class BoardController {
+    private BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
 
     @GetMapping("/")
     public String list(){
@@ -14,6 +22,11 @@ public class BoardController {
     @GetMapping("/post")
     public String post(){
         return "board/post.html";
+    }
+
+    @PostMapping("/post")
+    public String write(BoardDto boardDto){
+        return "redirect:/";
     }
 }
 
