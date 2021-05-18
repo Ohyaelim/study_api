7e0659b4b577c0cd8bad4ce819313030bc71e562
Ohyaelim <npr05080@gmail.com>
2021-05-18 09:05:52 +0900

    게시물 목록 구현을 위한 service 수정

--- src/main/java/yl/yaelim/board/domain/service/BoardService.java
+++ src/main/java/yl/yaelim/board/domain/service/BoardService.java
@@ -2,9 +2,12 @@ package yl.yaelim.board.domain.service;
 
 import org.springframework.stereotype.Service;
 import yl.yaelim.board.domain.dto.BoardDto;
+import yl.yaelim.board.domain.entity.Board;
 import yl.yaelim.board.domain.repository.BoardRepository;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class BoardService {
@@ -17,5 +20,23 @@ public class BoardService {
     @Transactional
     public Long savePost(BoardDto boardDto) {
         return boardRepository.save(boardDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public List<BoardDto> getBoardList(){
+        List<Board> boardList = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for (Board board : boardList){
+            BoardDto boardDto = BoardDto.builder()
+                    .id(board.getId())
+                    .author(board.getAuthor())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .createdDate(board.getCreatedDate())
+                    .build();
+            boardDtoList.add(boardDto);
+        }
+        return boardDtoList;
     }
 }
